396cb3b72bc3179041391c4f5f1611902f1084a7
Naveen <naveen.enock@gmail.com>
2025-06-21 19:29:06 -0400

    update

--- getdata.py
+++ getdata.py
@@ -13,3 +13,5 @@ while True:
     data, _ = sock.recvfrom(1024)
     x, y, z, yaw, pitch, roll = struct.unpack('<6d', data[:48])
     print(f"Yaw: {yaw:.0f}, Pitch: {pitch:.0f}, Roll: {roll:.0f} | X: {x:.0f}, Y: {y:.0f}, Z: {z:.0f}")
+
+
